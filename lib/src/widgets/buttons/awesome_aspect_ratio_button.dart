190ad4ed8b2bffa10cca4270ca3592a6e424312a
Célian Frasca <celian.fras@gmail.com>
2024-07-10 18:19:37 +0200

    Fonctionnement entier IOS

--- lib/src/widgets/buttons/awesome_aspect_ratio_button.dart
+++ lib/src/widgets/buttons/awesome_aspect_ratio_button.dart
@@ -11,7 +11,7 @@ class AwesomeAspectRatioButton extends StatelessWidget {
   final AwesomeTheme? theme;
   final Widget Function(CameraAspectRatios aspectRatio) iconBuilder;
   final void Function(SensorConfig sensorConfig, CameraAspectRatios aspectRatio)
-      onAspectRatioTap;
+  onAspectRatioTap;
 
   AwesomeAspectRatioButton({
     super.key,
@@ -19,57 +19,57 @@ class AwesomeAspectRatioButton extends StatelessWidget {
     this.theme,
     Widget Function(CameraAspectRatios aspectRatio)? iconBuilder,
     void Function(SensorConfig sensorConfig, CameraAspectRatios aspectRatio)?
-        onAspectRatioTap,
+    onAspectRatioTap,
   })  : iconBuilder = iconBuilder ??
-            ((aspectRatio) {
-              final AssetImage icon;
-              double width;
-              switch (aspectRatio) {
-                case CameraAspectRatios.ratio_16_9:
-                  width = 32;
-                  icon = const AssetImage(
-                      "packages/camerawesome/assets/icons/16_9.png");
-                  break;
-                case CameraAspectRatios.ratio_4_3:
-                  width = 24;
-                  icon = const AssetImage(
-                      "packages/camerawesome/assets/icons/4_3.png");
-                  break;
-                case CameraAspectRatios.ratio_1_1:
-                  width = 24;
-                  icon = const AssetImage(
-                      "packages/camerawesome/assets/icons/1_1.png");
-                  break;
-              }
+      ((aspectRatio) {
+        final AssetImage icon;
+        double width;
+        switch (aspectRatio) {
+          case CameraAspectRatios.ratio_16_9:
+            width = 32;
+            icon = const AssetImage(
+                "packages/camerawesome/assets/icons/16_9.png");
+            break;
+          case CameraAspectRatios.ratio_4_3:
+            width = 24;
+            icon = const AssetImage(
+                "packages/camerawesome/assets/icons/4_3.png");
+            break;
+          case CameraAspectRatios.ratio_1_1:
+            width = 24;
+            icon = const AssetImage(
+                "packages/camerawesome/assets/icons/1_1.png");
+            break;
+        }
 
-              return Builder(builder: (context) {
-                final iconSize = theme?.buttonTheme.iconSize ??
-                    AwesomeThemeProvider.of(context).theme.buttonTheme.iconSize;
+        return Builder(builder: (context) {
+          final iconSize = theme?.buttonTheme.iconSize ??
+              AwesomeThemeProvider.of(context).theme.buttonTheme.iconSize;
 
-                final scaleRatio = iconSize / AwesomeButtonTheme.baseIconSize;
-                return AwesomeCircleWidget(
-                  theme: theme,
-                  child: Center(
-                    child: SizedBox(
-                      width: iconSize,
-                      height: iconSize,
-                      child: FittedBox(
-                        child: Builder(
-                          builder: (context) => Image(
-                            image: icon,
-                            color: AwesomeThemeProvider.of(context)
-                                .theme
-                                .buttonTheme
-                                .foregroundColor,
-                            width: width * scaleRatio,
-                          ),
-                        ),
-                      ),
+          final scaleRatio = iconSize / AwesomeButtonTheme.baseIconSize;
+          return AwesomeCircleWidget(
+            theme: theme,
+            child: Center(
+              child: SizedBox(
+                width: iconSize,
+                height: iconSize,
+                child: FittedBox(
+                  child: Builder(
+                    builder: (context) => Image(
+                      image: icon,
+                      color: AwesomeThemeProvider.of(context)
+                          .theme
+                          .buttonTheme
+                          .foregroundColor,
+                      width: width * scaleRatio,
                     ),
                   ),
-                );
-              });
-            }),
+                ),
+              ),
+            ),
+          );
+        });
+      }),
         onAspectRatioTap = onAspectRatioTap ??
             ((sensorConfig, aspectRatio) => sensorConfig.switchCameraRatio());
 
@@ -95,7 +95,7 @@ class AwesomeAspectRatioButton extends StatelessWidget {
               rotateWithDevice: theme.buttonTheme.rotateWithCamera,
               child: theme.buttonTheme.buttonBuilder(
                 iconBuilder(snapshot.requireData),
-                () => onAspectRatioTap(sensorConfig, snapshot.requireData),
+                    () => onAspectRatioTap(sensorConfig, snapshot.requireData),
               ),
             );
           },
